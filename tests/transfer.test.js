2688e6b5836529f72e4d6b05726788a56489e668
Marco Realino <marcoarealino@gmail.com>
2026-04-16 20:35:43 -0300

    Adicionando o arquivo de transfer

---
+++ tests/transfer.test.js
@@ -0,0 +1,66 @@
+const { expect } = require('chai');
+const request = require('supertest');
+
+describe('Transfer testing', () => {
+
+  describe('POST /transferencias', () => {
+
+    it('sucesso, 201 ', async () => {
+      //Capturar o token
+      const responseLogin = await request('http://localhost:3000')
+              .post('/login')
+              .set('Content-Type', 'application/json')
+              .send({
+                'username': 'julio.lima',
+                'senha': '123456'
+              })
+
+      const token = responseLogin.body.token
+
+      // Fazendo a transferência        
+      const response = await request('http://localhost:3000')
+      .post('/transferencias')
+      .set('Content-Type', "application/json")
+      .set('Authorization', `Bearer ${token}`) // Usar o token capturado
+      .send(
+          {
+            'contaOrigem': 1,
+            'contaDestino': 2,
+            'valor': 11,
+            'token': ''
+      });
+
+      expect(response.status).to.equal(201);
+      console.log(response.body);
+    })
+
+    it('falha (valor < 10), 422 ', async () => {
+ //Capturar o token
+      const responseLogin = await request('http://localhost:3000')
+              .post('/login')
+              .set('Content-Type', 'application/json')
+              .send({
+                'username': 'julio.lima',
+                'senha': '123456'
+              })
+
+      const token = responseLogin.body.token
+
+      // Fazendo a transferência        
+      const response = await request('http://localhost:3000')
+      .post('/transferencias')
+      .set('Content-Type', "application/json")
+      .set('Authorization', `Bearer ${token}`) // Usar o token capturado
+      .send(
+          {
+            'contaOrigem': 1,
+            'contaDestino': 2,
+            'valor': 9.99,
+            'token': ''
+      });
+
+      expect(response.status).to.equal(422);
+      console.log(response.body);
+    })
+  })
+})
